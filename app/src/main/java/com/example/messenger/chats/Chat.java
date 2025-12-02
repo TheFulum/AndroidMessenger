9686fc44d1a0785ade1c9cf4ed08b152b6e1ea82
Daniil <lipouski.daniil@gmail.com>
2025-12-02 20:47:36 +0300

    update

--- app/src/main/java/com/example/messenger/chats/Chat.java
+++ app/src/main/java/com/example/messenger/chats/Chat.java
@@ -4,14 +4,22 @@ public class Chat {
 
     private String user1;
     private String user2;
+    private String lastActivity; // Добавьте это поле
 
     public Chat() {
-        // нужен Firebase
+        // нужен для Firebase
     }
 
     public Chat(String user1, String user2) {
         this.user1 = user1;
         this.user2 = user2;
+        this.lastActivity = ""; // Инициализируем пустой строкой
+    }
+
+    public Chat(String user1, String user2, String lastActivity) {
+        this.user1 = user1;
+        this.user2 = user2;
+        this.lastActivity = lastActivity;
     }
 
     public String getUser1() {
@@ -28,5 +36,13 @@ public class Chat {
 
     public void setUser2(String user2) {
         this.user2 = user2;
+    }
+
+    public String getLastActivity() {
+        return lastActivity;
+    }
+
+    public void setLastActivity(String lastActivity) {
+        this.lastActivity = lastActivity;
     }
 }
